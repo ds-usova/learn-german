4992886c5c25198e83fe788684f7bf86c9f7f66c
ds-usova <ds.usova@mail.ru>
2024-02-27 20:03:26 +0100

    Delete main page

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,8 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
-import Main from "../pages/MainPage.vue";
+import Dictionary from "../pages/DictionaryPage.vue";
 
 const routes = [
-    {path: "/", name: "main", component: Main},
+    {path: "/", name: "main", component: Dictionary},
     {path: "/dictionary", name: "dictionary", component: () => import('../pages/DictionaryPage.vue')},
     {path: "/practice", name: "practiceList", component: () => import('../pages/PracticeListPage.vue')},
     {path: "/practice/:id", name: "practice", component: () => import('../pages/PracticePage.vue'), props: true},
